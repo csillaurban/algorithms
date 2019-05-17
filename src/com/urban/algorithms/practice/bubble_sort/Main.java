1428750ba371d7a85f0d01d8fd4630e65705d2cd
Csilla Urban <csillaurban@gmail.com>
2019-05-17 21:19:42 +0200

    bubble sort

---
+++ src/com/urban/algorithms/practice/bubble_sort/Main.java
@@ -0,0 +1,12 @@
+package com.urban.algorithms.practice.bubble_sort;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Bubble bubbleSort = new Bubble();
+        int[] array = new int[] {12, 5, 20, 3, 1};
+        bubbleSort.sort(array);
+        System.out.println("sorted array: " + Arrays.toString(array));
+    }
+}
