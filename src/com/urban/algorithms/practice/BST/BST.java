0e8f2f3b4a6a9ba1052508d7cd4648eb13978868
Csilla Urban <csillaurban@gmail.com>
2019-05-19 21:01:31 +0200

    create Node

---
+++ src/com/urban/algorithms/practice/BST/BST.java
@@ -0,0 +1,7 @@
+package com.urban.algorithms.practice.BST;
+
+public class BST {
+
+}
+
+
---
+++ src/com/urban/algorithms/practice/BST/Node.java
@@ -0,0 +1,14 @@
+package com.urban.algorithms.practice.BST;
+
+public class Node {
+
+    private int value;
+    protected Node leftNode;
+    protected Node rightNode;
+
+    public Node(int value) {
+        this.value = value;
+        this.leftNode = null;
+        this.rightNode = null;
+    }
+}
