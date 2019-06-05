6d8e826c9bd64032f786b6ad86f17f27a80a01f6
Csilla Urban <csillaurban@gmail.com>
2019-06-05 17:11:33 +0200

    solve reverse num exercise

---
+++ src/com/urban/algorithms/practice/exercises/reverse_num/Main.java
@@ -0,0 +1,8 @@
+package com.urban.algorithms.practice.exercises.reverse_num;
+
+public class Main {
+    public static void main(String[] args) {
+        ReverseNum reverseNum = new ReverseNum();
+        reverseNum.reverse(1534236469);
+    }
+}
---
+++ src/com/urban/algorithms/practice/exercises/reverse_num/ReverseNum.java
@@ -0,0 +1,28 @@
+package com.urban.algorithms.practice.exercises.reverse_num;
+
+import java.util.Arrays;
+
+public class ReverseNum {
+    public int reverse(int x) {
+        int reversed = 0;
+
+        int num = x;
+        int prevNum = 0;
+
+        while(num!=0) {
+            int digit = num % 10;
+
+            reversed = reversed * 10 + digit;
+            if((reversed - digit) / 10 != prevNum) {
+                return 0;
+            }
+            prevNum = reversed;
+            num /= 10;
+
+
+        }
+
+        return reversed;
+
+    }
+}
